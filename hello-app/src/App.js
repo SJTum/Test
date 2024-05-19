9dc8b9de0855eb97d2dbf65824efd1b93bf4dc73
Saowanit Junsukon <saowanitjun@cpall.co.th>
2024-05-19 13:03:40 +0700

    First time to commit

---
+++ hello-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Helloworld from "./Helloworld";
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ^_^
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
